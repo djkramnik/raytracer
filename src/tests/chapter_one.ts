be679831a4ac0ef5f006e54e121f86a5c2cf65f2
David Gurr <davidk.gurr@gmail.com>
2024-01-04 14:49:18 -0500

    first tests work

--- src/tests/chapter_one.ts
+++ src/tests/chapter_one.ts
@@ -11,14 +11,14 @@ export const chapterOne = (testPrefix?: string) => {
   expect(tupleA.w).toBe(1.0)
   expect(isPoint(tupleA)).toBe(true)
   expect(isVector(tupleA)).toBe(false)
-  console.log(`${testPrefix}: tupleA tests passed`)
+  console.log(`${testPrefix}: tupleA tests passed!`)
 
   const tupleB = tuple(4.3, -4.2, 3.1, 0)
   expect(tupleB.x).toBe(4.3)
   expect(tupleB.y).toBe(-4.2)
   expect(tupleB.z).toBe(3.1)
-  expect(tupleB.w).toBe(1.0)
+  expect(tupleB.w).toBe(0.0)
   expect(isPoint(tupleB)).toBe(false)
   expect(isVector(tupleB)).toBe(true)
-  console.log(`${testPrefix}: tupleB tests passed`)
+  console.log(`${testPrefix}: tupleB tests passed!`)
 }
